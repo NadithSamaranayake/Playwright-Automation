c6696d42b05790c14434013aa04d807f3b1f9468
Nadith Samaranayake <nadith.jsamaranayake@gmail.com>
2025-10-16 16:56:39 +0530

    feat: implement login component with dynamic form switching and enhanced styling

--- playwright-client/src/app/login-component/login-component.ts
+++ playwright-client/src/app/login-component/login-component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-login-component',
-  imports: [],
+  imports: [CommonModule, NgClass],
   templateUrl: './login-component.html',
-  styleUrl: './login-component.scss'
+  styleUrl: './login-component.scss',
 })
 export class LoginComponent {
 
+  activeForm: "signIn" | "signUp" = "signIn";
+
+  setActiveForm(form: "signIn" | "signUp") {
+    this.activeForm = form;
+  }
 }
