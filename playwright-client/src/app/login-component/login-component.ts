47cc03f6d00dccaeccae3360a8d6c57980ec5980
Nadith Samaranayake <nadith.jsamaranayake@gmail.com>
2025-10-17 18:41:50 +0530

    feat: enhance login component with form validation, password visibility toggle, and Supabase integration

--- playwright-client/src/app/login-component/login-component.ts
+++ playwright-client/src/app/login-component/login-component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgClass } from '@angular/common';
+import { FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
-  imports: [CommonModule, NgClass],
+  imports: [CommonModule, NgClass, ReactiveFormsModule, FormsModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
 export class LoginComponent {
 
+  name = new FormControl(''); 
+
+  passwordVisible: boolean = false;
+
   activeForm: "signIn" | "signUp" = "signIn";
 
   setActiveForm(form: "signIn" | "signUp") {
     this.activeForm = form;
+  }
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  onSubmit(form: any){
+    alert('Form submitted!');
+    console.log(form.value);
   }
 }
